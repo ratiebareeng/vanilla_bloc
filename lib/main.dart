920f6e4770e5c8ce083851aff3feca3d298f18a5
Ratie Bareeng <bareengbiz@gmail.com>
2023-10-19 17:17:25 +0200

    Action centre positioning

--- lib/main.dart
+++ lib/main.dart
@@ -66,6 +66,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
         children: [
           FloatingActionButton(
             onPressed: _incrementCounter,
